c3bfa2cb7fe770e7e82202ace8e24d305db77aee
tormod17 <tormodsmith@gmail.com>
2016-03-01 23:50:52 +0000

    updating port

--- server.js
+++ server.js
@@ -5,15 +5,18 @@ var fs = require('fs');
 
 const server = new Hapi.Server();
 
-var serverOptions = {
-      port:(process.env.PORT || 8000),
-      host: host
-};
-
-server.connection({ port:4000});
 
 var host ='localhost';
 if(process.env.PORT) host = '0.0.0.0';
+var port =process.env.PORT || 8000
+var serverOptions = {
+      port:port,
+      host: host
+};
+
+server.connection({ port:port});
+
+
 
 
 
